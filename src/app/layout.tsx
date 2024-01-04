4d7f00a92ab34a37e967d4b0521a70cff11f5049
VIP <88263706+UFAPRO888s@users.noreply.github.com>
2024-01-05 06:21:38 +0700

    xz

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Noto_Sans_Thai } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const NotoSansThai = Noto_Sans_Thai({ subsets: ['thai'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="th">
+      <body className={NotoSansThai.className}>{children}</body>
     </html>
   )
 }
